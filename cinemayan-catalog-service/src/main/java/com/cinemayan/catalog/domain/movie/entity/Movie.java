c9252511b4c331202e79cedf5892507e9a8cda3c
Arivan Amin <arivanamin@gmail.com>
2026-05-22 21:13:00 +0300

    Add movie domain fields

--- cinemayan-catalog-service/src/main/java/com/cinemayan/catalog/domain/movie/entity/Movie.java
+++ cinemayan-catalog-service/src/main/java/com/cinemayan/catalog/domain/movie/entity/Movie.java
@@ -1,11 +1,30 @@
 package com.cinemayan.catalog.domain.movie.entity;
 
-import lombok.Value;
+import com.cinemayan.catalog.domain.content.*;
+import lombok.*;
 
+import java.time.*;
+import java.util.Set;
 import java.util.UUID;
 
-@Value
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Movie {
 
-    UUID id;
+    private UUID id;
+    private String title;
+    private String synopsis;
+    private Year releaseYear;
+    private Duration runtime;
+    private Set<Genre> genres;
+    private AgeRating ageRating;
+    private Set<ContentWarning> contentWarnings;
+    private Set<ProductionCompany> productionCompanies;
+    private String posterUrl;
+    private String trailerUrl;
+    private String imdbId;
+
+    private Instant createdAt;
+    private Instant updatedAt;
 }
